2a7bf93f555c769b4511fe99e0501dc251995a08
VIJAY KUMAR <vijaykumarchagantipati2002@gmail.com>
2024-09-16 21:05:17 +0530

    1.Bug fix in hospital admin on Duplicate Entries Created in Survey List with same speciality within the site level of the hospital. 2.Bug fix in hospital admin on Incorrect Username Generation if Spaces present for Manage Providers firstName and lastName.

--- dashboard/Survey-App/app.js
+++ dashboard/Survey-App/app.js
@@ -63,25 +63,6 @@ function checkAuth(req, res, next) {
 
 
 
-// In the GET / route
-// app.get('/', checkAuth, async (req, res) => {
-//     try {
-//         const db = client.db('manage_doctors');
-//         const collection = db.collection('surveys');
-        
-//         // Get hospital_code and site_code from the session
-//         const { hospital_code, site_code } = req.session.user;
-        
-//         // Find surveys that match both hospital_code and site_code
-//         const surveys = await collection.find({ hospital_code, site_code }).toArray();
-        
-//         // Render the index.ejs view, passing the filtered surveys
-//         res.render('index', { surveys });
-//     } catch (e) {
-//         console.error('Error getting surveys:', e);
-//         res.status(500).send('Internal Server Error');
-//     }
-// });
 
 app.get('/', checkAuth, async (req, res) => {
     try {
@@ -104,41 +85,6 @@ app.get('/', checkAuth, async (req, res) => {
 
 
 
-// app.get('/add', checkAuth, async (req, res) => {
-//     try {
-//         const db = client.db('surveyDB');
-//         const collection = db.collection('surveys');
-//         const surveys = await collection.find().toArray();
-        
-//         // Get hospital_code and site_code from the session
-//         const { hospital_code, site_code } = req.session.user;
-        
-//         // Pass hospital_code and site_code to the template
-//         res.render('add_survey', { surveys, hospital_code, site_code });
-//     } catch (e) {
-//         console.error('Error getting surveys:', e);
-//         res.status(500).send('Internal Server Error');
-//     }
-// });
-
-
-// app.get('/add', checkAuth, async (req, res) => {
-//     try {
-//         const db = client.db('surveyDB');
-//         const collection = db.collection('surveys');
-//         const customSurveys = await collection.find().toArray();
-        
-//         // Get hospital_code and site_code from the session
-//         const { hospital_code, site_code } = req.session.user;
-
-//         // Pass both the custom and API surveys to the template
-//         res.render('add_survey', { customSurveys, apiSurveys: surveysData, hospital_code, site_code });
-//     } catch (e) {
-//         console.error('Error getting surveys:', e);
-//         res.status(500).send('Internal Server Error');
-//     }
-// });
-
 app.get('/add', checkAuth, async (req, res) => {
     try {
         const db = client.db('surveyDB');
@@ -158,23 +104,35 @@ app.get('/add', checkAuth, async (req, res) => {
 
 
 
-// // In the POST /add route
+
 // app.post('/add', checkAuth, async (req, res) => {
 //     try {
 //         const db = client.db('manage_doctors');
 //         const collection = db.collection('surveys');
-        
-//         // Extract survey data, hospital code, and site code from the request body
-//         const { surveyName, specialty, hospital_code, site_code } = req.body;
-        
-//         // Insert the new survey with the hospital code and site code into the database
-//         await collection.insertOne({ surveyName, specialty, hospital_code, site_code });
+
+//         // Extract survey data from the request body
+//         const { specialty, apiSurveyData, customSurveyData, hospital_code, site_code } = req.body;
+
+//         // Parse the JSON strings into JavaScript objects
+//         const API = JSON.parse(apiSurveyData); // API surveys will have name and id
+//         const custom = JSON.parse(customSurveyData); // Custom surveys will only have names
+
+//         // Insert the new survey data into the database
+//         await collection.insertOne({
+//             specialty,
+//             API, // Rename apiSurveys to API
+//             custom, // Rename customSurveys to custom
+//             hospital_code,
+//             site_code
+//         });
+
 //         res.redirect('/');
 //     } catch (e) {
 //         console.error('Error adding survey:', e);
 //         res.status(500).send('Internal Server Error');
 //     }
 // });
+
 
 app.post('/add', checkAuth, async (req, res) => {
     try {
@@ -184,20 +142,33 @@ app.post('/add', checkAuth, async (req, res) => {
         // Extract survey data from the request body
         const { specialty, apiSurveyData, customSurveyData, hospital_code, site_code } = req.body;
 
-        // Parse the JSON strings into JavaScript objects
-        const API = JSON.parse(apiSurveyData); // API surveys will have name and id
-        const custom = JSON.parse(customSurveyData); // Custom surveys will only have names
+        // Check if a survey with the same specialty, hospital_code, and site_code already exists
+        const existingSurvey = await collection.findOne({
+            specialty: specialty,
+            hospital_code: hospital_code,
+            site_code: site_code
+        });
 
-        // Insert the new survey data into the database
+        if (existingSurvey) {
+            // If survey already exists, show an error message and redirect back to /add
+            req.flash('error', 'This specialty already exists for the selected hospital and site.');
+            return res.redirect('/add');
+        }
+
+        // Insert the new survey data if no duplication found
+        const API = JSON.parse(apiSurveyData);
+        const custom = JSON.parse(customSurveyData);
+
         await collection.insertOne({
             specialty,
-            API, // Rename apiSurveys to API
-            custom, // Rename customSurveys to custom
+            API,
+            custom,
             hospital_code,
             site_code
         });
 
-        res.redirect('/');
+        req.flash('success', 'Survey added successfully.');
+        return res.redirect('/add');
     } catch (e) {
         console.error('Error adding survey:', e);
         res.status(500).send('Internal Server Error');
@@ -207,100 +178,6 @@ app.post('/add', checkAuth, async (req, res) => {
 
 
 
-
-
-// // In the GET /edit/:id route
-// app.get('/edit/:id', checkAuth, async (req, res) => {
-//     try {
-//         const db1 = client.db('manage_doctors'); // For fetching the survey being edited
-//         const collection1 = db1.collection('surveys');
-//         const survey = await collection1.findOne({ _id: new ObjectId(req.params.id) });
-
-//         // Fetch all existing specialties from the surveyDB collection
-//         const db2 = client.db('surveyDB');
-//         const collection2 = db2.collection('surveys');
-//         const surveys = await collection2.find().toArray();
-//         const specialties = await collection1.distinct('specialty');
-
-//         // Pass survey, specialties, surveys, and site_code to the template
-//         res.render('edit_survey', { survey, specialties, surveys, site_code: survey.site_code });
-//     } catch (e) {
-//         console.error('Error getting survey for edit:', e);
-//         res.status(500).send('Internal Server Error');
-//     }
-// });
-
-
-// app.get('/edit/:id', checkAuth, async (req, res) => {
-//     try {
-//         const db = client.db('manage_doctors');
-//         const collection = db.collection('surveys');
-
-//         // Find the survey being edited using the provided ID
-//         const survey = await collection.findOne({ _id: new ObjectId(req.params.id) });
-
-//         // Fetch all available API and Custom surveys from the relevant collections
-//         const customSurveys = await db.collection('customSurveys').distinct('surveyName'); 
-//         const apiSurveys = await db.collection('apiSurveys').find().toArray();
-
-//         // Pass the survey to be edited, the available custom surveys, and API surveys to the view
-//         res.render('edit_survey', { survey, customSurveys, apiSurveys });
-//     } catch (e) {
-//         console.error('Error fetching survey for edit:', e);
-//         res.status(500).send('Internal Server Error');
-//     }
-// });
-
-
-// app.get('/edit/:id', checkAuth, async (req, res) => {
-//     try {
-//         const db = client.db('manage_doctors');
-//         const collection = db.collection('surveys');
-
-//         // Find the survey being edited using the provided ID
-//         const survey = await collection.findOne({ _id: new ObjectId(req.params.id) });
-
-//         // Fetch all available Custom surveys from the relevant collection
-//         const customSurveys = await db.collection('customSurveys').distinct('surveyName'); 
-        
-//         // Load API surveys from the surveys.json file
-//         const apiSurveys = surveysData;  // This comes from reading surveys.json
-
-//         // Pass the survey to be edited, the available custom surveys, and API surveys to the view
-//         res.render('edit_survey', { survey, customSurveys, apiSurveys });
-//     } catch (e) {
-//         console.error('Error fetching survey for edit:', e);
-//         res.status(500).send('Internal Server Error');
-//     }
-// });
-// app.get('/edit/:id', checkAuth, async (req, res) => {
-//     try {
-//         const db1 = client.db('surveyDB');  // Database for all surveys
-//         const db2 = client.db('manage_doctors');  // Database for selected surveys
-
-//         // Fetch all custom surveys from surveyDB.surveys collection
-//         const allSurveys = await db1.collection('surveys').find().toArray(); 
-
-//         // Find the survey being edited using the provided ID from manage_doctors.surveys collection
-//         const survey = await db2.collection('surveys').findOne({ _id: new ObjectId(req.params.id) });
-
-//         // Check if the survey exists
-//         if (!survey) {
-//             // If no survey found, redirect or show an error
-//             req.flash('error', 'Survey not found');
-//             return res.redirect('/');
-//         }
-
-//         // Load API surveys from the surveys.json file (if applicable)
-//         const apiSurveys = surveysData;  // Assuming this is your JSON source for API surveys
-
-//         // Pass the survey (for pre-checked values), all surveys (from surveyDB), and API surveys to the view
-//         res.render('edit_survey', { survey, allSurveys, apiSurveys });
-//     } catch (e) {
-//         console.error('Error fetching survey for edit:', e);
-//         res.status(500).send('Internal Server Error');
-//     }
-// });
 
 app.get('/edit/:id', checkAuth, async (req, res) => {
     try {
@@ -338,36 +215,6 @@ app.get('/edit/:id', checkAuth, async (req, res) => {
 
 
 
-
-
-
-// // In the POST /edit/:id route
-// app.post('/edit/:id', checkAuth, async (req, res) => {
-//     try {
-//         const db = client.db('manage_doctors');
-//         const collection = db.collection('surveys');
-        
-//         // Extract selected survey names, specialty, hospital code, and site code from the request body
-//         const { surveyNames, specialty, hospital_code, site_code } = req.body;
-
-//         // Update the survey document in the database with the new values
-//         await collection.updateOne(
-//             { _id: new ObjectId(req.params.id) },
-//             { $set: { 
-//                 surveyName: Array.isArray(surveyNames) ? surveyNames : [surveyNames], 
-//                 specialty, 
-//                 hospital_code, 
-//                 site_code  // Ensure site_code is also updated
-//             } }
-//         );
-
-//         // Redirect the user to the home page after the update is complete
-//         res.redirect('/');
-//     } catch (e) {
-//         console.error('Error editing survey:', e);
-//         res.status(500).send('Internal Server Error');
-//     }
-// });
 
 app.post('/edit/:id', checkAuth, async (req, res) => {
     try {
