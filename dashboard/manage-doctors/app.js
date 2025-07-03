b489793766523f26a09ed8de0ab246349063bc5e
Vijay Kumar <vijaykumarchagantipati2002@gmail.com>
2025-07-03 10:48:40 +0530

    hospital admin http -> https in url in surveyapp, manage-doctors, and env file.

--- dashboard/manage-doctors/app.js
+++ dashboard/manage-doctors/app.js
@@ -140,7 +140,7 @@ app.use((err, req, res, next) => {
 // Start the server
 function startServer() {
     app.listen(PORT, () => {
-        console.log(`Server is running on http://app.wehealthify.org${basePath}`);
+        console.log(`Server is running on https://app.wehealthify.org${basePath}`);
     });
 }
 
--- proms/PROMIS_API_IMPLEMENTATION/js/db-js.js
+++ proms/PROMIS_API_IMPLEMENTATION/js/db-js.js
@@ -1,5 +1,5 @@
 var Server = "https://www.assessmentcenter.net/ac_api/2014-01/Forms";
-var CORS_PROXY = "http://app.wehealthify.org:8081/";
+var CORS_PROXY = "https://app.wehealthify.org:8081/";
 var globalAssessmentID = ""; // Global variable to store the assessment ID
 var ItemResponseOID = "";    // Global variable to store the item response ID
 var Response = "";           // Global variable to store the response value
